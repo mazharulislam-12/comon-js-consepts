45f4a5dfb953c3049ab3c54a96fd2bb2b2da3f4f
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-24 10:04:27 -0700

    type of js

---
+++ types.js
@@ -0,0 +1,25 @@
+//primitive type
+let x = 5;
+let y = 59;
+console.log(x, y);
+y = 7;
+
+/**
+ * 1. numbers
+ * 2. string
+ * 3. boolean
+ *  
+ */
+// 1. null
+//2. undefined
+ 
+
+
+//non primitive type 
+const age = [45, 85, 63, 23];
+const student = {id: 1, class: 12};
+/**
+ * 1. array 
+ * 2. object
+ */
+ 
